af3cb04d1525a946bd17bd9e08fdbe4de5066314
vic-1998 <victor.mplaza98@gmail.com>
2021-04-03 18:37:10 -0500

    Client configuration file.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#  Client configuration file (w/ Puppet)a
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config'
+  line => '    IdentityFile ~/.ssh/holberton'
+}
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config'
+  line => '    PasswordAuthentication no'
+}
