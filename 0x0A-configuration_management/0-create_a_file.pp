990d6a034e3731a32a7cecd0b25df28386d36544
vic-1998 <victor.mplaza98@gmail.com>
2021-04-04 11:14:00 -0500

    Create a file.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+# Create a file
+
+file { 'holberton':
+  ensure  => 'present',
+  path    => '/tmp/holberton',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  content => 'I love Puppet',
+}
